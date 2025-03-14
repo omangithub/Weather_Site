465f15526a2ff04c5dda2655175b572f67aa8e1d
oman <smithowain@hotmail.com>
2025-03-14 12:00:57 +0900

    weather reports from tokyo functioning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css"
 
+/* two different ways of accessing the weather api with asynchronous coding
+
 async function londonWeather (url) {
     const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=BWMQYXAY8TAW5VKZ9DGNDN9GT')
     
@@ -7,4 +9,21 @@ async function londonWeather (url) {
     console.log(data);
 }
 
-londonWeather();
+async function londonWeather2 (url) {
+    const response = fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=BWMQYXAY8TAW5VKZ9DGNDN9GT')
+      .then ((response) => response.json())  
+      .then((data)=>{console.log(data)})  
+}
+
+console.log(londonWeather());
+console.log(londonWeather2());
+
+*/
+
+async function londonWeather (location) {
+    const response = fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Tokyo,Japan?key=BWMQYXAY8TAW5VKZ9DGNDN9GT')
+      .then ((response) => response.json())  
+      .then((data)=>{console.log(data)})  
+}
+
+console.log(londonWeather());
